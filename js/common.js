33c38021cda0f490546bc213ceb28023ab45fca8
oliegjio <oliegjio@gmail.com>
2017-10-01 18:30:06 +0300

    Commit

---
+++ js/common.js
@@ -0,0 +1,13 @@
+$('.top-carousel').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true
+});
+
+$('.bottom-carousel').slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true
+})
